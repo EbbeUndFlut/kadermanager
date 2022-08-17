6fad20997463df5167e9b9702b7bc856002cff36
Christian <c.peters@makermaker.de>
2022-08-17 16:20:04 +0200

    In process

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,37 @@
+const kaderContainer = document.getElementsByClassName('kader')[0]
+const teamContainer = document.getElementsByClassName('aufstellung')[0]
+const nameInput = document.querySelector('input')
 
+let kader = []
+let team = []
 
+const buildPage = () => {
+    clearContainer()
+    kader.forEach((item) => {
+        kaderContainer.appendChild(playerElement(item))
+    })
+    team.forEach((item) => {
+        teamContainer.appendChild(playerElement(item))
+    })
+}
+const addPlayer = () => {
+    kader.push(nameInput.value)
+    buildPage()
+}
+const removePlayer = () => {
+    console.log('first')
+    let index = kader.indexOf(nameInput.value)
+    console.log(index)
+    kader.splice(index, 1)
+    buildPage()
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const clearContainer = () => {
+    kaderContainer.innerHTML = ''
+    const heading = document.createElement('h2')
+    heading.innerHTML = 'Kader'
+    kaderContainer.appendChild(heading)
+}
 /**
  * Funktion die ein HTML Element erstellt und zurück gibt. 
  * Bitte nutzen um die Spieler anzuzeigen
@@ -34,3 +47,5 @@ const playerElement = (playerName) => {
 
     return container
 }
+
+buildPage()
